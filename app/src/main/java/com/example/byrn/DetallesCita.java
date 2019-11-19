572f75cd65038a8c417bf34722f5438c6f08a04c
Ilich Arredondo <ilichh1@hotmail.com>
2019-11-19 08:12:11 -0600

    [WIP] Added appointment detail logic and API

--- app/src/main/java/com/example/byrn/DetallesCita.java
+++ app/src/main/java/com/example/byrn/DetallesCita.java
@@ -5,29 +5,77 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import mx.reel.pojos.Appointment;
 
 public class DetallesCita extends AppCompatActivity {
     Button btnRegresar;
     private Appointment passedAppointment = null;
+    // View fields
+    private TextView appointmentIdLabel = null;
+    private TextView appointmentDateTimeLabel = null;
+    private TextView appointmentTypeLabel = null;
+    private TextView appointmentLocationLabel = null;
+    private TextView appointmentStatusLabel = null;
+    private TextView appointmentClientLabel = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalles_cita);
-
-        btnRegresar = (Button) findViewById(R.id.btnRegresarCita);
-
+        // Labels
+        appointmentIdLabel = findViewById(R.id.appointmentId);
+        appointmentDateTimeLabel = findViewById(R.id.appointmentDateTime);
+        appointmentTypeLabel = findViewById(R.id.appointmentType);
+        appointmentLocationLabel = findViewById(R.id.appointmentLocation);
+        appointmentStatusLabel = findViewById(R.id.appointmentStatus);
+        appointmentClientLabel = findViewById(R.id.appointmentClient);
+        // Back button
+        btnRegresar = findViewById(R.id.btnRegresarCita);
         btnRegresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onBackPressed();
             }
         });
-        // Aquí me quedé: Crear el layout para esta activity
-        // TODO: Appointment detail layout
         passedAppointment = (Appointment) getIntent().getSerializableExtra("appointment");
-        System.out.println(passedAppointment.getId());
+        initViewData();
+
+        // Client detail listener
+        appointmentClientLabel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClientDetailAction();
+            }
+        });
+    }
+
+    private void initViewData() {
+        appointmentIdLabel.setText(passedAppointment.getId() + "");
+        appointmentDateTimeLabel.setText(
+                passedAppointment.getDate() + " a las "
+                + passedAppointment.getStartTime()
+        );
+        appointmentTypeLabel.setText(passedAppointment.getAppoimentType());
+        appointmentLocationLabel.setText(passedAppointment.getLocation());
+        String status;
+        switch (passedAppointment.getStatus()) {
+            case "2":
+                status = "Aceptada";
+            break;
+            case "3":
+                status = "Rechazada";
+            break;
+            default:
+                status = "Pendiente";
+        }
+        appointmentStatusLabel.setText(status);
+        appointmentClientLabel.setText(passedAppointment.getCustomerId() + "");
+    }
+
+    public void onClientDetailAction() {
+        // TODO: Add client detail layout & logic
+        System.out.println("TODO: Add client detail layout & logic");
     }
 }
